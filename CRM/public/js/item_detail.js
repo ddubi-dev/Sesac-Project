9e7058971cddbf28dd689651255deb797ae9ca2a
ddubi-dev <1kimc@naver.com>
2024-11-24 23:39:53 +0900

    feat: item detail chart

--- CRM/public/js/item_detail.js
+++ CRM/public/js/item_detail.js
@@ -73,4 +73,77 @@ async function fetchSales(itemId) {
     }
     tableBody.appendChild(bodyRow);
   });
+
+  // 월간 매출액 그래프
+  drawGraph(data);
+}
+
+// 월간 매출액 그래프
+async function drawGraph(data) {
+  // 왼쪽 : 매출액
+  // 오른쪽 : 아이템 개수
+  // bottom : 날짜
+  // Total Revenue, 월별 매출액
+  // item count, 아이템 개수
+  // Month, Total_Revenue, Item_Count
+
+  const labels = data.map((d) => d.Month);
+  const revenue = data.map((d) => d["Total Revenue"]);
+  const count = data.map((d) => d["Item Count"]);
+
+  console.log("labels: ", labels);
+  console.log("revenue: ", revenue);
+  console.log("count: ", count);
+
+  const chart = document.getElementById("chart").getContext("2d");
+  new Chart(chart, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Total Revenue",
+          data: revenue,
+          backgroundColor: "rgba(255, 99, 132, 0.5)", // 막대 색상
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          yAxisID: "y", // 첫 번째 축에 연결
+        },
+        {
+          label: "Item Count",
+          data: count,
+          type: "line", // 선형 차트
+          borderColor: "rgba(54, 162, 235, 1)", // 선 색상
+          backgroundColor: "rgba(54, 162, 235, 0.5)",
+          borderWidth: 2,
+          tension: 0.4, // 선의 곡선 정도
+          yAxisID: "y1", // 두 번째 축에 연결
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          type: "linear",
+          position: "left",
+          title: {
+            display: true,
+            text: "총 매출액",
+          },
+        },
+        y1: {
+          type: "linear",
+          position: "right",
+          title: {
+            display: true,
+            text: "아이템 개수",
+          },
+          grid: {
+            drawOnChartArea: false, // 두 번째 축의 그리드 제거
+          },
+        },
+      },
+    },
+  });
 }
