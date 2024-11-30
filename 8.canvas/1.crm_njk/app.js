ee4a34f3cfc51062eca5551d1cdbde896b8ea485
ddubi-dev <1kimc@naver.com>
2024-11-30 17:21:37 +0900

    Feat: 월별매출액과 아이템 개수 chart

--- 8.canvas/1.crm_njk/app.js
+++ 8.canvas/1.crm_njk/app.js
@@ -8,11 +8,20 @@ const port = 3000;
 // 눈적스 초기화
 nunjucks.configure("views", {
   autoescape: true,
+  // HTML을 자동으로 이스케이프 하도록 지정.
   express: app,
+  // express 애플리케이션에 nunjucks 연결. 이를 통해 nunjucks 템플릿을 express의 응답처리에 사용 가능
 });
+// 템플릿 엔진 설정 함수
+// views->템플릿 파일들이 저장된 디렉토리의 경로 지정
+// nunjucks는 이 경로를 통해 템플릿 파일들을 찾아 렌더링
+
+app.set("view engine", "njk");
+// express에서 템플릿 엔진으로 눈적스를 사용하고, 파일 확장자를 .njk
+// Express가 템플릿을 렌더링할 때, views 폴더 내의 .njk 파일을 사용
 
 // app.set("view engine", "html"); // 눈적스 기본 확장자 njk -> html로 변경
-app.set("view engine", "njk");
+// 기본적으로 Nunjucks 템플릿 엔진의 확장자를 .njk로 사용하지만, 확장자를 .html로 변경하려는 코드
 
 app.get("/", (req, res) => {
   console.log("여기야");
@@ -54,16 +63,10 @@ app.get("/", (req, res) => {
 
         res.render("monthly_revenue", {
           rows,
-          labels: JSON.stringify(labels),
-          revenues: JSON.stringify(revenues),
+          // labels: JSON.stringify(labels),
+          // revenues: JSON.stringify(revenues),
         });
       }
-
-      // REST-API =>
-      // 프론트엔드에서 1. 캔바스 크리기 2. 캔바스에 데이터를 넣고 바 그래프를 그린다 3. 넣을 데이터는? => 요청(fetch(), GET)
-      // fetch(GET --->)
-      //             <--- 응답의 형태, 데이터 포맷, JSON
-      // 백엔드에서
     }
   );
 
