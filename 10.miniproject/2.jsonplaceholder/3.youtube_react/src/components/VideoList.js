efe26767eff0ef739f892075a8342b5bc38e6ec1
ddubi-dev <1kimc@naver.com>
2024-12-10 01:19:54 +0900

    feat: 조회수 추가

--- 10.miniproject/2.jsonplaceholder/3.youtube_react/src/components/VideoList.js
+++ 10.miniproject/2.jsonplaceholder/3.youtube_react/src/components/VideoList.js
@@ -1,4 +1,36 @@
+import { useState, useEffect } from "react";
+import ViewCount from "./ViewCount";
+
 const VideoList = ({ videos }) => {
+  const [viewCount, setViewCount] = useState({}); // viewCount는 객체로 관리
+
+  useEffect(() => {
+    // viewCount가 변경될 때마다 해당 조회수를 가진 비디오 목록을 업데이트합니다.
+    if (Object.keys(viewCount).length > 0) {
+      const updatedVideos = videos.map((video) => ({
+        title: video.snippet.title,
+        viewCount: parseInt(viewCount[video.id.videoId]) || 0, // videoId에 해당하는 조회수
+        videoId: video.id.videoId,
+        description: video.snippet.description,
+      }));
+
+      console.table(updatedVideos); // 비디오 목록 출력
+    }
+  }, [viewCount, videos]);
+
+  // useEffect(() => {
+  //   if (viewCount !== null) {
+  //     const updatedVideos = videos.map((video) => ({
+  //       title: video.snippet.title,
+  //       viewCount: viewCount,
+  //       videoId: video.id.videoId,
+  //       description: video.snippet.description,
+  //     }));
+
+  //     console.table(updatedVideos);
+  //   }
+  // }, [viewCount]);
+
   return (
     <div>
       <ul>
@@ -8,10 +40,18 @@ const VideoList = ({ videos }) => {
             <div>
               <h3>{video.snippet.title}</h3>
               <p>{video.snippet.description}</p>
+              <ViewCount
+                videoId={video.id.videoId}
+                onViewCountFetched={(count) => {
+                  setViewCount((prevCount) => ({
+                    ...prevCount,
+                    [video.id.videoId]: count, // videoId에 해당하는 조회수 업데이트
+                  }));
+                }}
+              ></ViewCount>
             </div>
           </li>
         ))}
-        <li></li>
       </ul>
     </div>
   );
